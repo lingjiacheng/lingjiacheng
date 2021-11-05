26c0d501cf6d9ad4c0597d821dd9d72083a50cb6
lingjiacheng <826039917@qq.com>
2021-11-05 13:24:21 +0800

    作业

--- 第三次作业/level2.js
+++ 第三次作业/level2.js
@@ -1,9 +1,13 @@
 'use strict';
 let arr = [[1, [2, 3], 4], [5, [6, 7], 8, 9]];
-  function flat(arr){
-    return arr.reduce((prev,next) => {
-        return prev.concat((next instanceof Array) ? flat(next):next)
-     },[])
-  }
- console.log(flat(arr));
-console.log(arr)
+function fn(array) {
+  array.forEach((item,index) =>{
+    if(item instanceof Array){
+        arr = arr.concat(fn(item))
+    }else{
+        arr.push(item)
+    }
+})
+return arr
+}
+console.log(fn(arr))
