5dc3055e9cb64bd61a775acfe78eb402966a2726
lingjiacheng <826039917@qq.com>
2021-11-12 22:22:18 +0800

    作业

--- 第四次作业/level1.js
+++ 第四次作业/level1.js
@@ -1,7 +1,16 @@
+// function factorial(n){
+//     if (n == 1)
+//       return n
+//     else
+//       return n* factorial(n-1)
+// }
+// alert(factorial(6))
 function factorial(n){
-    if (n == 1)
-      return n
-    else
-      return n* factorial(n-1)
+  let i=n;
+  let a=i;
+  while(i>1){
+    a=a*--i;
+  }
+  return a
 }
 alert(factorial(6))
